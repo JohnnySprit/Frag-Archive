f6dcf5aa246679a7219386bf9973e5aaabaf0fcb
JohnnySprit <flanegt@gmail.com>
2025-09-11 00:07:59 -0400

    first commit

---
+++ frag-archive/src/main/java/com/cs/frag_archive/Player/Player.java
@@ -0,0 +1,7 @@
+package com.cs.frag_archive.Player;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Player {
+}
---
+++ frag-archive/src/test/java/com/cs/frag_archive/FragArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.frag_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
